8305ee9b753a62836ebc7750bcedd0997a80fe9a
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-10-25 16:59:55 +0200

    quest

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 class Distance:
 
     def __init__(self, km: float) -> None:
-        self.km = km
+        self.km: float = km
 
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
@@ -17,7 +17,7 @@ class Distance:
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
-        return None
+        return NotImplemented
 
     def __iadd__(self, other: "Distance | int | float") -> "Distance":
         if isinstance(other, Distance):
@@ -33,16 +33,26 @@ class Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: "Distance | int | float") -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
         return self.km < other
 
     def __gt__(self, other: "Distance | int | float") -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
         return self.km > other
 
     def __eq__(self, other: "Distance | int | float") -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
         return self.km == other
 
     def __le__(self, other: "Distance | int | float") -> bool:
+        if isinstance(other, Distance):
+            return self.km <= other.km
         return self.km <= other
 
     def __ge__(self, other: "Distance | int | float") -> bool:
+        if isinstance(other, Distance):
+            return self.km >= other.km
         return self.km >= other
