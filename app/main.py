e78e2255a1f127273cf3de792fbf2a25b2153a6b
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-10-25 16:44:28 +0200

    quest

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Distance:
 
     def __init__(self, km: float) -> None:
@@ -9,7 +12,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int) -> "Distance | None":
+    def __add__(self, other: int) -> "Distance | int | float":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
